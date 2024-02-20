ba9a1c0693cbd8e654c22e974a08a84a9c953094
Mardin <mardinomer001@gmail.com>
2024-02-20 12:41:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// index.js
+
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 400 && ride <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (ride > 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+module.exports = scuberGreetingForFeet;
+
+
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+module.exports = { ternaryCheckCity, switchOnCharmFromTip };
+
+
+// index.js
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+
+module.exports = switchOnCharmFromTip;
